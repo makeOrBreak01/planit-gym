19cc8f6ae0c097d4bb2407d3db67115a8f6dd9bc
Stefan Miller <stefanmiller739@gmail.com>
2023-06-22 09:28:06 -0400

    barebones sign in page

---
+++ src/pages/login.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function LoginPage() {
+    return(
+        <div className="signIn">
+            <input placeholder="username / email..." />
+            <input placeholder="password..." />
+        </div>
+    )
+}
